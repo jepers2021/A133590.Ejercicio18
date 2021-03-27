f869edee96ee575dadbfda8ac9fa1f8b51a555ed
Jorge <jepers2021@gmail.com>
2021-03-27 19:01:04 -0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace A133590.Ejercicio18
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ejercicio 18");
+            Console.WriteLine("Dado un número, lo redondea a 2 decimales.");
+            Console.Write("Por favor, ingrese un número: ");
+            double numero;
+            bool exito = Double.TryParse(Console.ReadLine(), out numero);
+            if (!exito)
+            {
+                Console.WriteLine("Ingreso inválido, no puede ser interpretado como numérico.");
+                return;
+            }
+
+            Console.WriteLine($"Número redondeado a dos decimales: {Math.Round(numero,2)}");
+
+
+        }
+    }
+}
